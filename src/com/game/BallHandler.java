6a2760085b71f914e760a9623e51d7b47b685a3d
go6o12344 <72763513+go6o12344@users.noreply.github.com>
2021-07-21 19:55:49 +0300

    Cosmetic update to BallHandler constructor

--- src/com/game/BallHandler.java
+++ src/com/game/BallHandler.java
@@ -16,21 +16,21 @@ public class BallHandler {
     }
 
     public BallHandler() throws IOException {
-        balls.add(new Ball(new Vector2D(150, 180), new Vector2D(0,0), 11));
+        balls.add(new Ball(new Vector2D(210, 270), new Vector2D(0, 0),1));
         balls.add(new Ball(new Vector2D(150, 210), new Vector2D(0,0), 2));
-        balls.add(new Ball(new Vector2D(150, 240), new Vector2D(0, 0),13));
+        balls.add(new Ball(new Vector2D(180, 255), new Vector2D(0, 0),3));
         balls.add(new Ball(new Vector2D(150, 270), new Vector2D(0, 0),4));
         balls.add(new Ball(new Vector2D(150, 300), new Vector2D(0, 0),5));
         balls.add(new Ball(new Vector2D(180, 195), new Vector2D(0, 0),6));
+        balls.add(new Ball(new Vector2D(240, 225), new Vector2D(0, 0),7));
+        balls.add(new Ball(new Vector2D(210, 240), new Vector2D(0, 0),8));
+        balls.add(new Ball(new Vector2D(270, 240), new Vector2D(0, 0),9));
         balls.add(new Ball(new Vector2D(180, 225), new Vector2D(0, 0),10));
-        balls.add(new Ball(new Vector2D(180, 255), new Vector2D(0, 0),3));
+        balls.add(new Ball(new Vector2D(150, 180), new Vector2D(0,0), 11));
+        balls.add(new Ball(new Vector2D(240, 255), new Vector2D(0, 0),12));
+        balls.add(new Ball(new Vector2D(150, 240), new Vector2D(0, 0),13));
         balls.add(new Ball(new Vector2D(180, 285), new Vector2D(0, 0),14));
         balls.add(new Ball(new Vector2D(210, 210), new Vector2D(0, 0),15));
-        balls.add(new Ball(new Vector2D(210, 270), new Vector2D(0, 0),1));
-        balls.add(new Ball(new Vector2D(240, 225), new Vector2D(0, 0),7));
-        balls.add(new Ball(new Vector2D(240, 255), new Vector2D(0, 0),12));
-        balls.add(new Ball(new Vector2D(270, 240), new Vector2D(0, 0),9));
-        balls.add(new Ball(new Vector2D(210, 240), new Vector2D(0, 0),8));
     }
 
     public void tick(){
@@ -46,9 +46,10 @@ public class BallHandler {
     }
 
     public void render(Graphics g){
-        for(Ball ball : balls)
+        for(Ball ball : balls) {
             ball.render(g);
-        System.out.println(balls.get(0).position);
+            System.out.println(ball);
+        }
     }
 
 }
