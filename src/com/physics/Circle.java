25b7611aa776916480a5eebd667e109d592013e3
go6o12344 <72763513+go6o12344@users.noreply.github.com>
2021-07-23 19:14:26 +0300

    Implemented a basic circle class

---
+++ src/com/physics/Circle.java
@@ -0,0 +1,25 @@
+package com.physics;
+
+public class Circle {
+    public double radius;
+    public Vector2D center;
+
+    public Circle(double radius, Vector2D center) {
+        this.radius = radius;
+        this.center = center;
+    }
+
+    public static boolean between(double a, double b1, double b2){
+        if( a == b1 && b1 == b2) return false;
+        return (b1 <= a && b2 >= a) || (b1 >= a && b2 <= a);
+    }
+
+    public boolean checkForIntersection(Segment s){
+        double ls = new Vector2D(s.start).subtract(center).length(), le = new Vector2D(s.end).subtract(center).length();
+        if(ls < radius && le < radius) return false;
+        if(ls <= radius) return true;
+        if(le <= radius) return true;
+        if(s.perpendicularLength(center) <= radius && (between(center.x, s.start.x, s.end.x) || between(center.y, s.start.y, s.end.y))) return true;
+        return false;
+    }
+}
