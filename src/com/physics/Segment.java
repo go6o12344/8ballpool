1def190493a9fa43dd851018d4bc5cb600268993
go6o12344 <72763513+go6o12344@users.noreply.github.com>
2021-07-23 19:15:26 +0300

    Update Segment.java

--- src/com/physics/Segment.java
+++ src/com/physics/Segment.java
@@ -7,4 +7,17 @@ public class Segment {
         this.start = start;
         this.end = end;
     }
+
+    public double perpendicularLength(Vector2D v){
+        double a, b;
+        //find line eq.
+        if(start.x != end.x){
+            a = (end.y - start.y) / (end.x - start.x);
+            b = start.y - a * start.x;
+        }
+        else{
+            return v.x - start.x;
+        }
+        return Math.abs(a * v.x - v.y + b) / Math.sqrt(a * a + 1);
+    }
 }
